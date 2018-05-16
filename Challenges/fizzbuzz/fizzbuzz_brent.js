003c8649e50e89d192300c130b9f9d3c0b715581
Brent Connor <wmconnor1986@gmail.com>
2018-05-16 13:19:30 -0400

    Count instances of fizz, buzz, fizzbuzz, and indivisible

--- Challenges/fizzbuzz/fizzbuzz_brent.js
+++ Challenges/fizzbuzz/fizzbuzz_brent.js
@@ -20,24 +20,65 @@ function fizzbuzz() {
   }
 }
 
-fizzbuzz();
+// fizzbuzz();
 
-console.log('##############');
-console.log('##############');
-console.log('##############');
-console.log('##############');
-console.log('##############');
-console.log('##############');
+// console.log('##############');
+// console.log('##############');
+// console.log('##############');
+// console.log('##############');
+// console.log('##############');
+// console.log('##############');
 
 // solution from https://stackoverflow.com/a/20647069/3954106
 
 function fizzbuzz_two() {
   for(i = 1; i < 101; i++) {
     var result = "";
-    if (i % 3 === 0) result += "Fizz";
-    if (i % 5 === 0) result += "Buzz";
+    if (!(i % 3)) result += "Fizz";
+    if (!(i % 5)) result += "Buzz";
     console.log(result || i);
   }
 }
 
-fizzbuzz_two();
+// fizzbuzz_two();
+
+
+function fizzbuzz_and_count() {
+  var fizzCount = 0,
+    buzzCount = 0,
+    fizzBuzzCount = 0,
+    count = 0;
+
+  for(i = 1; i < 101; i++) {
+    var modulus3 = i % 3,
+      modulus5 = i % 5;
+
+    if(modulus3 != 0 && modulus5 != 0) {
+      console.log(i);
+      ++count;
+    }
+    else if(modulus3 === 0 && modulus5 === 0) {
+      console.log("FizzBuzz");
+      ++fizzBuzzCount;
+    }
+    else if(modulus3 === 0) {
+      console.log("Fizz");
+      ++fizzCount;
+    }
+    else if(modulus5 === 0) {
+      console.log("Buzz");
+      ++buzzCount;
+    }
+  }
+  console.log('<><><><><><><><><><><><>');
+  console.log("Number of FizzBuzz");
+  console.log(fizzBuzzCount);
+  console.log("Number of Fizz");
+  console.log(fizzCount);
+  console.log("Number of Buzz");
+  console.log(buzzCount);
+  console.log("Number of indivisible");
+  console.log(count);
+}
+
+fizzbuzz_and_count();
