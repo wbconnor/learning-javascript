feeba679b297589be43f368fdb6277cb58122d22
stephendp <stephendp51@gmail.com>
2018-05-19 07:37:35 -0400

    refactored version

--- Challenges/fizzbuzz/fizzbuzz_steve.js
+++ Challenges/fizzbuzz/fizzbuzz_steve.js
@@ -1,5 +1,6 @@
+
 function fizzbuzz (value) {
-  var answerSet = ['FuzzBuzz', 'Buzz', 'Fuzz', i]
+  var answerSet = ['FuzzBuzz', 'Buzz', 'Fuzz']
     for (var i = 1; i <= value; i++) {
         answerSet[3] = i
         j = (i % 3) ? 1 : 0
@@ -8,3 +9,13 @@ function fizzbuzz (value) {
     }
 }
 fizzbuzz(20)
+
+function fizzbuzz2 (value) {
+    var answerSet = ['FuzzBuzz', 'Buzz', 'Fuzz']
+      for (var i = 1; i <= value; i++) {
+          answerSet[3] = i
+          console.log(`${answerSet[((i % 3) ? 1 : 0) + ((i % 5) ? 2 : 0)]}`)
+      }
+  }
+
+  fizzbuzz2(30)
--- fizzbuzz.js
+++
@@ -1,9 +0,0 @@
-function fizzbuzz (value) {
-  var answerSet = ['FuzzBuzz', 'Buzz', 'Fuzz']
-    for (var i = 1; i <= value; i++) {
-        j = (i % 3) ? 1 : 0
-        k = (i % 5) ? 2 : 0
-        console.log(j+k === 3 ? i : `${answerSet[j+k]}`)
-    }
-}
-fizzbuzz(20)
