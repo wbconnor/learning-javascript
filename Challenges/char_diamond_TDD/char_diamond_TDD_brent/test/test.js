2b309f316dd2314c07624cd162c9294b965aa7b1
Brent Connor <wmconnor1986@gmail.com>
2018-06-13 12:40:33 -0400

    Formatting

--- Challenges/char_diamond_TDD/char_diamond_TDD_brent/test/test.js
+++ Challenges/char_diamond_TDD/char_diamond_TDD_brent/test/test.js
@@ -1,19 +1,18 @@
 var assert = require('assert');
 var diamond = require('../char_diamond');
 
-// Test for valid input
+  // Test for valid input
   it('Test for valid input', function() {
     assert.equal(!diamond("A"), false);
   });
-// Test for failing input
+  // Test for failing input
   it('Test for invalid input -- one number', function() {
     assert.equal(diamond(1), false);
   });
-
   // Test for failing input
-    it('Test for invalid input -- two letters', function() {
-      assert.equal(diamond("AA"), false);
-    });
+  it('Test for invalid input -- two letters', function() {
+    assert.equal(diamond("AA"), false);
+  });
 
 // Test that lowercase gets converted to uppercase
 
