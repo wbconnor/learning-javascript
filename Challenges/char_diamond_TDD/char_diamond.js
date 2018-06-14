42599fc5e9a57721bd1e444a186358f510327548
Brent Connor <wmconnor1986@gmail.com>
2018-06-14 13:27:42 -0400

    Begin the main char_diamond solution code

--- Challenges/char_diamond_TDD/char_diamond.js
+++ Challenges/char_diamond_TDD/char_diamond.js
@@ -1,10 +1,33 @@
 
-module.exports = function diamond(letter) {
+function diamond(letter) {
   pattern = /^[A-z]{1}$/;
   if(pattern.test(letter)) {
-    return letter = letter.toUpperCase()
+    let alphabet = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+     "W", "X", "Y", "Z" ];
+    letter = letter.toUpperCase();
+    let index = alphabet.indexOf(letter);
+    let result = "";
+    for(i = index; i >= 0; i--) {
+      for(j = 0; j < i; j++) {
+        result += " ";
+      }
+      result += alphabet[index - i];
+      if(index - i > 0) {
+        for(s = index - i; s > 0; s--) {
+          result += " ";
+        }
+        result += alphabet[index - i] + "\n";
+      }
+      else {
+        result += "\n";
+      }
+    }
+    console.log(result);
   }
   else {
     return false;
   }
 }
+
+diamond("d");
